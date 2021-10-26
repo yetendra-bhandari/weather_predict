f57e0db3abcd865da12620d66ab6c22d763b546c
Shoaib Asgar <shoaib.mca19.du@gmail.com>
2021-10-27 03:18:27 +0530

    second form page added

--- weather/urls.py
+++ weather/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
 
 from . import views
 
@@ -7,4 +9,11 @@ urlpatterns = [
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
     path('', views.home, name='home'),
+    path('app/', views.app, name='app'),
+    path('predict/', views.predict, name='predict'),
+    path('upload/', views.upload, name='upload'),
 ]
+
+# file upload
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- weather/views.py
+++ weather/views.py
@@ -9,9 +9,26 @@ def home(request):
     message = request.session.pop('message', None)
     return render(request, 'weather/index.html', {'message': message})
 
+def app(request):
+    message = request.session.pop('message', None)
+    return render(request, 'weather/app.html', {'message': message})
+
+
+def upload(request):
+    return HttpResponseRedirect(reverse('app'))
+
+def predict(request):
+    return HttpResponseRedirect(reverse('app'))
 
 def register(request):
-    return HttpResponse("You're at the registration page.")
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            request.session['message'] = 'Invalid Data entered!'
+        else:
+            request.session['message'] = 'Successfull! Login using same credentials'
+    return HttpResponseRedirect(reverse('home'))
 
 
 def login(request):
