3abed82c958a2951ccf033f736663fb99df37eab
Yetendra Bhandari <yetendrabhandari@gmail.com>
2021-10-27 13:04:55 +0530

    added model label

--- weather/models.py
+++ weather/models.py
@@ -5,3 +5,6 @@ class User(models.Model):
     name = models.CharField(max_length=64)
     email = models.EmailField(max_length=64, unique=True)
     password = models.CharField(max_length=64)
+
+    def __str__(self) -> str:
+        return self.email
