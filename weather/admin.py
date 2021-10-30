d83f828e963409b4f5ac59609c246927bbca36bd
Shoaib Asgar <shoaib.mca19.du@gmail.com>
2021-10-30 23:41:43 +0530

    chi2 fix

--- weather/admin.py
+++ weather/admin.py
@@ -2,4 +2,16 @@ from django.contrib import admin
 
 from .models import User, Data
 
-admin.site.register([User, Data])
+
+class DataAdmin(admin.ModelAdmin):
+    list_display = ['user', 'csvname']
+
+
+admin.site.register(Data, DataAdmin)
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email']
+
+
+admin.site.register(User, UserAdmin)
